e553dc5d5660cdad7ccd46d4cb083af74689f76e
Zain-ul-abideen-hashmi <09-131242-093@student.bahria.edu.pk>
2026-02-04 09:08:33 +0500

    DSA LAB 1 has been completetd

---
+++ DSA LAB 1 (Templates)/Template.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class House
+{
+
+private:
+    string owner;
+    string address;
+    int bedrooms;
+    float price;
+
+public:
+
+    void getHouseinfo();
+    void printHouseinfo();
+};
+
+
+void House::getHouseinfo()
+{
+    cout << "owner name: ";
+
+    getline(cin,owner);
+    cin.ignore();
+    cout << "adress of location: ";
+    getline(cin, address);
+
+    cout << "how many bedroom are inside?: ";
+    cin >> bedrooms;
+
+    cout << "cost?: ";
+    cin >> price;
+}
+
+
+void House::printHouseinfo()
+{
+    cout << owner << "\t\t"
+         << address << "\t\t"
+         << bedrooms << "\t\t"
+         << price << endl;
+}
+
+
+
+int main()
+{
+
+    House available[100];
+    int count = 0;
+    char choice;
+
+
+    do
+    {
+        if (count < 100)
+        {
+
+            available[count].getHouseinfo();
+
+            count++;
+
+            cout << " Do  You want to see another one ? (y or n): ";
+            cin >> choice;
+
+
+            cin.ignore();
+            cout << endl;
+        }
+
+        else
+
+        {
+            cout <<  "error "<< endl;
+            break;
+        }
+
+    }
+
+    while (choice == 'y' || choice == 'Y');
+
+
+
+
+
+    cout << "----------------------------------------------" << endl;
+
+
+
+
+    cout << "Owner\t\tAddress\t\tBedrooms\t\tPrice" << endl;
+
+    for (int i = 0; i < count; i++)
+    {
+
+        available[i].printHouseinfo();
+    }
+
+
+
+
+    cin.get();
+
+    return 0;
+
+
+
+}
