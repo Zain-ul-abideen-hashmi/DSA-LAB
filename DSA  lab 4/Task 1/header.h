d6637b3d676dad2eb4b1a03e68519d66e802c8c4
Zain-ul-abideen-hashmi <09-131242-093@student.bahria.edu.pk>
2026-02-16 12:30:38 +0500

    Example

---
+++ DSA  lab 4/Task 1/header.h
@@ -0,0 +1,8 @@
+//
+// Created by AL SHAHBAZ on 2/16/2026.
+//
+
+#ifndef DSA_LAB_HEADER_H
+#define DSA_LAB_HEADER_H
+
+#endif //DSA_LAB_HEADER_H
---
+++ DSA  lab 4/Task 1/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout<<"Hllo "<<endl;
+
+    cin.ignore();
+    cin.get();
+
+    return 0;
+
+}
